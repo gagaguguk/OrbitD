319d8967b22770fa8307f90b45281225197c7cc2
Gaga <116130695+gagaguguk@users.noreply.github.com>
2026-02-02 15:58:57 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/orbitdbchain/main.go
@@ -0,0 +1,23 @@
+// cmd/orbitdbchain/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "orbitdbchain/internal/orbitdbchain"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := orbitdbchain.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
